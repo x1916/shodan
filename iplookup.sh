45d5d1f8a5f600f76084744d81cb4175b23c7c3d
x1916 <x19164131@student.ncirl.ie>
2021-04-15 15:38:33 +0100

    Updated Thu 15 Apr 2021 15:38:33 IST

--- iplookup.sh
+++ iplookup.sh
@@ -15,11 +15,23 @@ fi
 IPADDRESS=$(nslookup $1 | grep -m2 "Address:" | tail -n1 | cut -d : -f 2 | awk '{$1=$1};1')
 
 
+echo "******************************"
+echo "**       Host Details       **"
+echo "******************************"
+
 shodan host $IPADDRESS
+
+echo "******************************"
+echo "**       Domain Details       **"
+echo "******************************"
+
+
 shodan domain $1
 shodan honeyscore $IPADDRESS
 
-echo "IP ADDRESS HISTORY"
+echo "******************************"
+echo "**    IP History Details    **"
+echo "******************************"
 
 shodan host --history $IPADDRESS
 
